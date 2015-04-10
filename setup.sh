8e849649ac4affb45c2ae38e1acc65a1cdec1564
zen <pierre.therrode@reezocar.com>
2015-04-10 17:17:04 +0200

    update

--- setup.sh
+++ setup.sh
@@ -116,6 +116,40 @@ then
 
 	ln -s /etc/apache2/mods-available/evasive.conf /etc/apache2/mods-enabled/evasive.conf
 	service apache2 restart
+
+	echo -e "\033[32m[+]  Install HTTPS \033[0m"
+	apt-get install -y openssl
+	a2enmod ssl
+	service apache2 restart
+	mkdir /etc/apache2/ssl
+	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.key -out /etc/apache2/ssl/apache.crt
+	chmod o-rw /etc/apache2/ssl
+	cp /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-available/save_default-ssl.conf
+	rm /etc/apache2/sites-available/default-ssl.conf
+	echo '<IfModule mod_ssl.c>
+    <VirtualHost _default_:443>
+        ServerAdmin admin@example.com
+        ServerName your_domain.com
+        ServerAlias www.your_domain.com
+        DocumentRoot /var/www/html
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+        SSLEngine on
+        SSLCertificateFile /etc/apache2/ssl/apache.crt
+        SSLCertificateKeyFile /etc/apache2/ssl/apache.key
+        <FilesMatch "\.(cgi|shtml|phtml|php)$">
+                        SSLOptions +StdEnvVars
+        </FilesMatch>
+        <Directory /usr/lib/cgi-bin>
+                        SSLOptions +StdEnvVars
+        </Directory>
+        BrowserMatch "MSIE [2-6]" \
+                        nokeepalive ssl-unclean-shutdown \
+                        downgrade-1.0 force-response-1.0
+        BrowserMatch "MSIE [17-9]" ssl-unclean-shutdown
+    </VirtualHost>
+</IfModule>' >> /etc/apache2/sites-available/default-ssl.conf
+service apache2 restart
 fi
 #----------------------Ending PHP + APACHE2 -------------------------------------------------
 
